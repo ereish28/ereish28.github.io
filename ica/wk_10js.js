8bcd8e6540a75ce39dd20652c6005ef2fdeb94af
ereish28 <ereish28@outlook.com>
2024-10-31 13:36:11 -0600

    more

--- ica/wk_10js.js
+++ ica/wk_10js.js
@@ -1,10 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let my_var = 100;
+    var my_other_var = "helo world";
+    const this_is_a_constant = "constant";
+    console.log(my_other_var);
+    my_other_var += "!";
 
 
+    console.log(document.getElementById("my_head"));
 
-console.log(document.getElementById("my_head"));
+    document.getElementById("my_head").addEventListener("click", function(e){
+        alert("clicked the h1");
 
-document.getElementById("my_head").addEventListener("click", function(e){
-    alert("clicked the h1");
-    document.getElementById("paragraph").style.color = "red";
-    document.getElementById("paragraph").innerHTML = "html text";
-})
+
+        console.log(document.getElementById("p"));
+
+        paragraph.addEventListener("mouseover", function(e) {
+            alert("hovering");
+        });
+
+        });
+    });
