c8d201f1ddc24f0d2cd32f6dc8791dbcfb91d16b
ereish28 <ereish28@outlook.com>
2024-10-31 13:10:14 -0600

    more changes

--- ica/wk_10js.js
+++ ica/wk_10js.js
@@ -1,1 +1,10 @@
-console.log("hello world")
+
+
+
+console.log(document.getElementById("my_head"));
+
+document.getElementById("my_head").addEventListener("click", function(e){
+    alert("clicked the h1");
+    document.getElementById("paragraph").style.color = "red";
+    document.getElementById("paragraph").innerHTML = "html text";
+})
