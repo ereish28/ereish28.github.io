fab162ad25c150c35842c2bfe6b5d34f7d298531
ereish28 <ereish28@outlook.com>
2024-11-07 10:14:10 -0700

    more changes

--- wa/wa11/script11.js
+++ wa/wa11/script11.js
@@ -5,8 +5,10 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-
 const images = ['IMG1.JPG', `IMG2.JPG`, `IMG3.JPG`, `IMG4.JPG`, `IMG5.JPG`];
+
+
+/* Declaring the alternative text for each image file */
 const alts = {
   'IMG1.JPG' : 'Image of a cathedral from a distance',
   'IMG2.JPG' : 'Cathedral in Brussels, Belgium',
@@ -15,11 +17,7 @@ const alts = {
   'IMG5.JPG' : 'Gold statue of a man on a horse'
 }
 
-/* Declaring the alternative text for each image file */
-
 /* Looping through images */
-
-
 for (const image of images) {
     const newImage = document.createElement('img');
     newImage.setAttribute('src', `images/${image}`);
@@ -31,15 +29,12 @@ for (const image of images) {
     });
   }
 
-
-
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
 newImage.setAttribute('alt', xxx);
 thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
-
 btn.addEventListener('click', () => {
     const btnClass = btn.getAttribute('class');
     if (btnClass === 'dark') {
