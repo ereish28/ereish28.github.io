f773a9a51707b65f325d8c280f0200e878ce3793
ereish28 <ereish28@outlook.com>
2024-11-14 10:16:50 -0700

    test

--- wa/wa12/wa12script.js
+++ wa/wa12/wa12script.js
@@ -2,7 +2,7 @@ let myData = {};
 
 function fetchData(){
     Math.floor(Math.random() * 3000);
-    fetch("https://corsproxy.io/?https://xkcd.com/${}/info.0.json")
+    fetch("https://corsproxy.io/?https://xkcd.com/(Math.random)/info.0.json")
 
     .then((res) => {
         if(res.ok) {
@@ -19,8 +19,7 @@ function fetchData(){
         console.log(myData);
         document.getElementById("comic").innerHTML = myData.title    //title
         document.getElementById("comic").src = myData.img; //displaying comic
-        document.getElementById("comic").alt = myData.alt   //alt text
-
+        document.getElementById("comic").alt = myData.alt   //alt tex
     })
 
 }
