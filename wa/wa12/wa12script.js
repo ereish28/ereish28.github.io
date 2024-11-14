ea301fbea935e4b33dff51ebc191a8ec8e4e1623
ereish28 <ereish28@outlook.com>
2024-11-14 09:58:38 -0700

    start

---
+++ wa/wa12/wa12script.js
@@ -0,0 +1,27 @@
+let myData = {};
+
+function fetchData(){
+    fetch("https://corsproxy.io/?https://xkcd.com/3000/info.0.json")
+
+    .then((res) => {
+        if(res.ok) {
+            return res.json();
+        }
+
+        else {
+            console.log(res);
+        }
+    })
+
+    .then ((res) => {
+        myData = res;
+        console.log(myData);
+        document.getElementById("comic").innerHTML = myData.title    //title
+        document.getElementById("comic").src = myData.img; //displaying comic
+        document.getElementById("comic").alt = myData.alt   //alt text
+
+    })
+
+}
+
+fetchData();
