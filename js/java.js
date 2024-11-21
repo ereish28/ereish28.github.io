a7ab4f519fea2a59d4236d9b22956fe29026001d
ereish28 <ereish28@outlook.com>
2024-11-21 10:38:35 -0700

    more

--- js/java.js
+++ js/java.js
@@ -1,1 +1,25 @@
-console.log("hello");
+fetch('../js/projectjson.json')
+    .then(response =>{
+        return response.json();
+    }).then(projects =>{
+        console.log(projects);
+        parseData(projects);
+    }).catch(err =>{
+        console.log(`{err}`)
+    })
+
+function parseDate(data) {
+    for(let i=0; i < data.projects.length; i++) {
+        document.getElementById("projects").innerHTML += `
+            <div class="row project" id="${data.projects[i].subdomain}">
+                <div class="projimg">
+                    <img src="images/insert image from object`>
+                </div>
+                <div class ="description">
+                    <h2>${data.projects[i].name}</h2> 
+                    <p class="subtitle">${data.projects[i].subtitle}</p>
+                    <p>${data.projects[i].abstract}</p>
+                </div>
+            </div>`;
+    }
+}
